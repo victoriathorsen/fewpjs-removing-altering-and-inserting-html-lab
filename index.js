e5f532216d6e2af5fc4146f4ab89d65dadf1fd08
Victoria <victoriathorsenn@gmail.com>
2021-06-01 01:45:49 -0400

    commit tree 4dd51b7e19c65ccf3e97f0fe4f5c8eb45b73d478

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+let  findMain = document.querySelector("#main")
+findMain.remove()
+
+let newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = ' Vic is the champion'
